19841081a3fe1926ceb496f90570ec029ac6c01e
APozas <alejandro.pozas@elektra.com.mx>
2023-05-09 10:04:51 -0600

    Se corrigen Interceptores para la API_KEY

---
+++ app/src/test/java/com/example/jetmoviesapp/data/local/WatchListDatabaseTest.kt
@@ -0,0 +1,54 @@
+package com.example.jetmoviesapp.data.local
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.jetmoviesapp.data.local.entities.MovieEntity
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WatchListDatabaseTest : TestCase() {
+
+    private lateinit var db: WatchListDatabase
+    private lateinit var dao: WatchListDao
+
+    @Before
+    public override fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, WatchListDatabase::class.java).build()
+        dao = db.watchListDao()
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun writeWatchListMovie() = runBlocking {
+        val movie = MovieEntity(
+            movieId = 100000,
+            title = "Prueba Test Unit",
+            originalTitle = "Unit test Movie",
+            posterPath = "Pagina Web",
+            voteAverage = 85.0,
+            runtime = 1,
+            overview = "Pelicula Dedicada a los Unit test",
+        )
+
+        dao.insert(movie)
+
+        val movies = dao.getMovieByIdFromLocal(10000)
+
+        if (movies != null) {
+            assert(movies == movie)
+        }
+    }
+}
