e7c34d3dbf235ec52d7bf6f8cecf444717ec531d
APozas <alejandro.pozas@elektra.com.mx>
2023-05-09 14:35:57 -0600

    Se agregan Unit test y dependencias para testing

--- app/src/test/java/com/example/jetmoviesapp/ui/presentation/watch_list/WatchListViewModelTest.kt
+++ app/src/test/java/com/example/jetmoviesapp/ui/presentation/watch_list/WatchListViewModelTest.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import coil.compose.AsyncImagePainter
 import com.example.jetmoviesapp.data.local.WatchListDao
 import com.example.jetmoviesapp.data.local.WatchListDatabase
 import com.example.jetmoviesapp.data.local.entities.MovieEntity
+import com.google.common.truth.Truth.assertThat
 import junit.framework.TestCase
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.isActive
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
@@ -18,7 +17,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class WatchListDatabaseTest2 : TestCase() {
+class WatchListDatabaseTest : TestCase() {
 
     private lateinit var db: WatchListDatabase
     private lateinit var dao: WatchListDao
@@ -37,8 +36,22 @@ class WatchListDatabaseTest2 : TestCase() {
     }
 
     @Test
-    fun getWatchListMovie() = runBlocking {
+    fun writeGetWatchListMovie() = runBlocking {
+        val movie = MovieEntity(
+            movieId = 100000,
+            title = "Prueba Test Unit",
+            originalTitle = "Unit test Movie",
+            posterPath = "Pagina Web",
+            voteAverage = 85.0,
+            runtime = 1,
+            overview = "Pelicula Dedicada a los Unit test",
+        )
+
+        dao.insert(movie)
+
         val movies = dao.getWatchList()
-        assert(movies.filterNotNull().equals(""))
+        val moviesL = movies.toList()
+
+        assertThat(moviesL.contains<Any>(movie)).isTrue()
     }
 }
