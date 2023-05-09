a21b215ee93637e082f35820b39526b7fd990706
APozas <alejandro.pozas@elektra.com.mx>
2023-05-09 11:54:14 -0600

    Se agregan Unit test

--- app/src/test/java/com/example/jetmoviesapp/ui/presentation/watch_list/WatchListViewModelTest.kt
+++ app/src/test/java/com/example/jetmoviesapp/ui/presentation/watch_list/WatchListViewModelTest.kt
@@ -1,20 +1,24 @@
-package com.example.jetmoviesapp.data.local
+package com.example.jetmoviesapp.ui.presentation.watch_list
 
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import coil.compose.AsyncImagePainter
+import com.example.jetmoviesapp.data.local.WatchListDao
+import com.example.jetmoviesapp.data.local.WatchListDatabase
 import com.example.jetmoviesapp.data.local.entities.MovieEntity
 import junit.framework.TestCase
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.runBlocking
 import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class WatchListDatabaseTest : TestCase() {
+class WatchListDatabaseTest2 : TestCase() {
 
     private lateinit var db: WatchListDatabase
     private lateinit var dao: WatchListDao
@@ -33,23 +37,8 @@ class WatchListDatabaseTest : TestCase() {
     }
 
     @Test
-    fun writeWatchListMovie() = runBlocking {
-        val movie = MovieEntity(
-            movieId = 100000,
-            title = "Prueba Test Unit",
-            originalTitle = "Unit test Movie",
-            posterPath = "Pagina Web",
-            voteAverage = 85.0,
-            runtime = 1,
-            overview = "Pelicula Dedicada a los Unit test",
-        )
-
-        dao.insert(movie)
-
-        val movies = dao.getMovieByIdFromLocal(10000)
-
-        if (movies != null) {
-            assert(movies == movie)
-        }
+    fun getWatchListMovie() = runBlocking {
+        val movies = dao.getWatchList()
+        assert(movies.filterNotNull().equals(""))
     }
 }
