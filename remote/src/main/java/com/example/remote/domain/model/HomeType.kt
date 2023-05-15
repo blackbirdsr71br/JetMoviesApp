9ac2e718326c7b1562da610acb5894a020c6660b
APozas <alejandro.pozas@elektra.com.mx>
2023-05-15 14:34:03 -0600

    Se modulariza local and remote Module

--- remote/src/main/java/com/example/remote/domain/model/HomeType.kt
+++
@@ -1,8 +0,0 @@
-package com.example.remote.domain.model
-
-import com.example.remote.remote.movie.Movie
-
-sealed class HomeType {
-    data class Popular(val popular: List<com.example.remote.remote.movie.Movie>) : HomeType()
-    data class TopRated(val topRated: List<com.example.remote.remote.movie.Movie>) : HomeType()
-}
